aec520db1544c42b34e8a41aad4d27b6b2cd5d0e
Ivan Fesenko <pharmivan@gmail.com>
2020-10-29 11:17:03 +0200

    show-hide fix

---
+++ src/js/show-hide.js
@@ -0,0 +1,10 @@
+export function hideElement(elem) {
+  elem.classList.add('is-hidden');
+}
+
+export function showElement(elem) {
+  if (elem.classList.contains('is-hidden')) {
+    elem.classList.remove('is-hidden');
+  }
+  return;
+}
